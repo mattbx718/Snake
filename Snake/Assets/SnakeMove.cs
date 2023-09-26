22c771529d8ce98fb98b0f849d2bac83166c0ae5
mattbx718 <144148087+mattbx718@users.noreply.github.com>
2023-09-26 12:16:15 -0400

    sep 26

--- Snake/Assets/SnakeMove.cs
+++ Snake/Assets/SnakeMove.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class SnakeMove : MonoBehaviour
 {
-    //variable
+    //variables
     private Vector2 direction; //control direction of movement
+
+    List<Transform> segments;    // variable to store all the parts if the body of the snake
+    public Transform bodyPrefab;   //variable to store the body 
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        segments = new List<Transform>();      //create a new list
+        segments.Add(transform);               // add the head of the snake to th list 
     }
 
     // Update is called once per frame
     void Update()
-    { 
+    {
         // change direction of the snake
         if (Input.GetKeyDown(KeyCode.W)) //when W key is pressed
         {
@@ -43,5 +48,21 @@ public class SnakeMove : MonoBehaviour
             Mathf.Round(this.transform.position.x) + direction.x, //round the number add value to x
             Mathf.Round(this.transform.position.y) + direction.y  // round the number add value to y
             );
+    }
+    //Function to make the snak egrow
+    void Grow()
+    {
+        Transform segment = Instantiate(this.bodyPrefab);         //create a new body part
+        segment.position = segments[segments.Count - 1].position;  // position it on the back of the snake 
+        segments.Add(segment);                                         // add it to the list
+    }
+
+    //Function for collision 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Food")
+        {
+            Grow();
+        }
     }
 }
